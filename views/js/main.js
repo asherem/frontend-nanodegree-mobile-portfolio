ff302177429c04d3c32eb2554614296b8b4d502f
alexeix <alexei.x.sheremet@gmail.com>
2017-06-06 21:39:30 -0400

    Minor fixes

--- views/js/main.js
+++ views/js/main.js
@@ -529,8 +529,7 @@ function updatePositions() {
 
     for (i = 0; i < items.length; i++) {
         var phase = constArray[i % 5];
-        var left = -items[i].basicLeft + 1000 * phase + 'px';
-                    items[i].style.transform = "translateX("+left+") translateZ(0)"; // faster compared to original code
+                    items[i].style.transform = 'translateX(' + 100 * phase + 'px)'; // faster compared to original code
     }
 
     window.performance.mark("mark_end_frame");
@@ -582,7 +581,7 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
-        elem.style.top = (Math.floor(i / cols) * s) + 'px';
+        elem.style.left = (i % cols) * s + 'px';
         document.getElementById("movingPizzas1").appendChild(elem);
     }
     updatePositions();
